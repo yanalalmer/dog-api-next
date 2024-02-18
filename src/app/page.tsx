33b5b0673aef7314eaedeb08a2462e2aa11ecdf6
yanalalmer@yahoo.com <yanalalmer@yahoo.com>
2024-02-19 00:29:03 +0100

    added card component and tailwind config for size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,20 @@ import { useGetRandomDog } from '@/hooks';
 export default function Home() {
   const url = 'https://dog.ceo/api/breeds/image/random';
   const { dog, loading, error, refetch } = useGetRandomDog(url);
-  console.log(dog.message);
+  if (loading) return <h1>Loading....</h1>;
+  if (error) return <h1>{error?.message}</h1>;
+  console.log(dog);
   return (
     <main className='flex justify-center items-center mt-16'>
       <Card
-        background={dog.message}
+        background={dog?.message}
         link=''
         title='this is my title asldfj;alskdf;laksjdfasd;lkfjasd;flkjasdfa;lsdkfa;sldkf'
         description='this is a description asosdfa;lsdkfjas;ldkfjas;ldfj;lkajsdf;laksjdf;laksjdf;lkasjdf'
       />
       {/* <button className='block' onClick={refetch}>
-        re fetch
-      </button> */}
+          re fetch
+        </button> */}
     </main>
   );
 }
--- src/hooks/useGetRandomDog.tsx
+++ src/hooks/useGetRandomDog.tsx
@@ -9,16 +9,15 @@ export const useGetRandomDog = (url: string) => {
   const [loading, setLoading] = useRecoilState<ILoading>(loadingState);
   const [dog, setDog] = useRecoilState<IDogData>(dogState);
   const [error, setError] = useRecoilState<IErrorResponse>(errorState);
-
   const runFetch = () => {
     setLoading({ loading: true });
     axios
       .get<IDogData>(url)
       .then((res) => setDog(res?.data))
       .catch((err) => {
-        setError(
-          err?.message || { message: 'An error occurred please try again' }
-        );
+        setError({
+          message: err?.message || 'An error occurred please try again',
+        });
       })
       .finally(() => {
         setLoading({ loading: false });

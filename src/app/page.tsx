ac27c6da08515620b9c57fd3c5eeca993dedc0cd
yanalalmer@yahoo.com <yanalalmer@yahoo.com>
2024-02-18 20:02:51 +0100

    added get request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+'use client';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { loadingState, dogState, errorState } from '@/state';
+import { useRecoilState } from 'recoil';
+
 export default function Home() {
+  const url = 'https://dog.ceo/api/breeds/image/random';
+  const [loading, setLoading] = useRecoilState(loadingState);
+  const [dog, setDog] = useRecoilState(dogState);
+  const [error, setError] = useRecoilState(errorState);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(url)
+      .then((res) => console.log(res))
+      .catch((err) => {
+        console.log('this is an error', err.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [url]);
   return <main>this is my app</main>;
 }

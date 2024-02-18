64eb590123070dc174696a478ee375d7bd1b1ce1
yanalalmer@yahoo.com <yanalalmer@yahoo.com>
2024-02-18 22:10:19 +0100

    added nav logo

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -1,10 +1,24 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export const Navbar = () => {
   return (
     <nav className='w-full flex items-center justify-between gap-3 px-3 py-3 rounded-2xl'>
-      <Link href='/'>navbar logo</Link>
+      <Link href='/'>
+        <div className='w-16 logo cursor-pointer flex items-center'>
+          <Image
+            className='rounded-3xl shadow-white-lg fill-black'
+            src={'/images/logo.svg'}
+            width={250}
+            height={250}
+            alt='logo'
+            priority
+          />
+          <p className='ml-4 font-bold hidden tablet:block'>Dog Generator</p>
+        </div>
+      </Link>
       <div className='flex items-center gap-3 relative'>
+        <Link href='/'>Home</Link>
         theme toggle button
       </div>
     </nav>

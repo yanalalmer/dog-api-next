4fac1af651257caa40daa6a5784f050a45f25912
yanalalmer@yahoo.com <yanalalmer@yahoo.com>
2024-02-20 10:31:17 +0100

    fixed capitalizing breed name

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,8 +2,6 @@ export const extractBreedName = (url: string) => {
   const parts = url.split('/');
   const breedPart = parts[parts.length - 2];
   const breedNameParts = breedPart.split('-');
-  const capitalizedBreedName = breedNameParts
-    .map((part) => part.charAt(0).toUpperCase() + part.slice(1))
-    .join(' ');
-  return capitalizedBreedName;
+  const breedName = breedNameParts.join(' ');
+  return breedName;
 };

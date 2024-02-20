bc7be801b32bda08428d16d1dc8270c0153e205a
yanalalmer@yahoo.com <yanalalmer@yahoo.com>
2024-02-20 11:14:54 +0100

    added data to breed page

--- src/app/(pages)/breed/[breedName]/page.tsx
+++ src/app/(pages)/breed/[breedName]/page.tsx
@@ -1,21 +1,29 @@
 'use client';
 import { useParams } from 'next/navigation';
 import { useGetBreedImages } from '@/hooks';
+import { Loading, Card, PrimaryButton } from '@/components';
 
 export default function BreedName() {
   const params = useParams();
   const breed = decodeURIComponent(params.breedName);
-  const url = `https://dog.ceo/api/${breed}/images`;
+  const url = `https://dog.ceo/api/breed/hound/images`;
   const {
     breed: { message: images, status: reqStatus },
     loading: { loading: isLoading },
     error: { message: errorMessage },
+    loadMore,
   } = useGetBreedImages(url);
-  console.log(images);
 
+  if (isLoading) return <Loading />;
+  if (errorMessage) return <h1>{errorMessage}</h1>;
   return (
-    <main>
-      <h1>this is my breed name page</h1>
+    <main className='grid grid-cols-3 grid-rows-auto gap-8'>
+      {images.map((image, index) => (
+        <Card background={image} title='' link='' key={index} description='' />
+      ))}
+      <div onClick={loadMore}>
+        <PrimaryButton type='submit' text='load more' />
+      </div>
     </main>
   );
 }
